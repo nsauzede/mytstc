5eb9d33af5e3e7dd3aa170a27399b398cc4d51b1
Nicolas Sauzede <nsauzede@laposte.net>
2021-02-19 02:15:22 +0100

    Fix tstc_new.v

--- tstc_new.v
+++ tstc_new.v
@@ -20,32 +20,33 @@ mut:
 	body []ASTNode
 }
 
-struct NumberLiteral {
-	ctx   &ASTNode = voidptr(0)
-	value string
-}
-
-struct CallExpression {
+struct Call {
 mut:
 	ctx    &ASTNode = voidptr(0)
 	name   string
 	params []ASTNode
 }
 
-struct ExpressionStatement {
-	ctx        &ASTNode = voidptr(0)
-	expression &ASTNode
+struct NumberLiteral {
+	value string
 }
 
-struct Call {
-	ctx &ASTNode = voidptr(0)
+struct ExpressionStatement {
+	expression ASTNode
+}
+
+struct CallExpression {
 mut:
-	@type     string
-	name      string
+	callee    ASTNode
 	arguments []ASTNode
 }
 
-type ASTNode = Call | CallExpression | ExpressionStatement | NumberLiteral | Program
+struct Identifier {
+	name string
+}
+
+type ASTNode = Call | CallExpression | ExpressionStatement | Identifier | NumberLiteral |
+	Program
 
 fn print_ast_r(node ASTNode, nest int) {
 	for i := 0; i < nest; i++ {
@@ -63,7 +64,7 @@ fn print_ast_r(node ASTNode, nest int) {
 			print('${typeof(node).name}')
 			println(' value=$node.value')
 		}
-		CallExpression {
+		Call {
 			print('${typeof(node).name}')
 			println(' name=$node.name params=\\')
 			for e in node.params {
@@ -74,14 +75,18 @@ fn print_ast_r(node ASTNode, nest int) {
 			print('${typeof(node).name}')
 			print_ast_r(node.expression, nest + 1)
 		}
-		Call {
+		CallExpression {
 			print('${typeof(node).name}')
-			print(' callee type=${node.@type}')
-			print(' name=$node.name')
-			println(' arguments=\\')
+			print(' callee=')
+			print_ast_r(node.callee, 0)
+			println(' arguments:$node.arguments.len=\\')
 			for e in node.arguments {
 				print_ast_r(e, nest + 1)
 			}
+		}
+		Identifier {
+			print('${typeof(node).name}')
+			print(' name=$node.name')
 		}
 	}
 }
@@ -158,7 +163,7 @@ fn walk(mut current_ MyInt, tokens []Token) &ASTNode {
 				mut current := current_
 				current.value++
 				name := tokens[current.value] as Name
-				mut node := &CallExpression{
+				mut node := &Call{
 					name: name.value
 				}
 				current.value++
@@ -190,8 +195,8 @@ fn walk(mut current_ MyInt, tokens []Token) &ASTNode {
 	panic('walk: Type error !')
 }
 
-fn parser(tokens []Token) &ASTNode {
-	mut ast := &Program{}
+fn parser(tokens []Token) ASTNode {
+	mut ast := Program{}
 	mut current := MyInt{}
 	for current.value < tokens.len {
 		mut node := &ASTNode{}
@@ -201,172 +206,100 @@ fn parser(tokens []Token) &ASTNode {
 	return ast
 }
 
-fn traverse_node(node ASTNode, mut parent ASTNode) ASTNode {
-	// println('traverse_node.. node=${voidptr(node)}')
-	println('traverse_node..')
-	if mut node is NumberLiteral {
-		println(' Numlit')
-		// if parent != voidptr(0) {
-		// if parent is Program {println('is progr')}
-		// if parent is CallExpression {println('is callexpr=${voidptr(&parent)}')}
-		nl := &NumberLiteral{
-			value: node.value
-		}
-		// println('parent ctx=${voidptr(ctx)} numlit=${voidptr(nl)} ${nl.value}')
-		if mut parent is Program {
-			// println('pushing numlit to progr ctx=${voidptr(&ctx)}')
-			println('pushing numlit to progr')
-			parent.body << nl
-		} else if mut parent is CallExpression {
-			// println('pushing numlit to callexpr ctx=${voidptr(&ctx)}')
-			println('pushing numlit to callexpr')
-			// println('ADDING NL=${voidptr(nl)} TO PARAMS')
-			parent.params << nl
-		} else if mut parent is Call {
-			// println('pushing numlit to call ctx=${voidptr(&ctx)}')
-			println('pushing numlit to call')
-			parent.arguments << nl
-		} else {
-			panic('parent ctx is unknown ?')
-		}
-		//} else {
-		// println('parent ctx is null ?')
-		//}
-		//} else {
-		// println('parent is null ?')
-		//}
-	}
-	//	exit(0)
-	if mut node is CallExpression {
-		println(' Callexpr')
-		mut expression := &Call{
-			@type: 'Identifier'
-			name: node.name
-		}
-		node.ctx = expression
-		if mut parent is CallExpression {
-			println('pushing call to callexpr expr=${voidptr(expression)}')
-			// println('ADDING EXPRESSION=${voidptr(expression)} TO PARAMS')
-			parent.params << expression
-		} else if mut parent is Program {
-			println('pushing call to progr')
-			expression2 := &ExpressionStatement{
-				expression: expression
-			}
-			parent.body << expression2
-		}
-	}
-	if mut node is Program {
-		// node.body<<NumberLiteral{value:"123"}
-		println(' traverse Prog ctx=${voidptr(node.ctx)} $node.body.len')
-		for i := 0; i < node.body.len; i++ {
-			// ret_ctx := 
-			node.body[i] = traverse_node(node.body[i], mut &node)
-			match node.body[i] {
-				Program { // node.body[i].ctx = rect_ctx 
-					// println('ctx=${voidptr(node.body[i].ctx)}')
+fn traverse_node(node ASTNode, parent &ASTNode) ASTNode {
+	if parent != voidptr(0) {
+		mut child := ASTNode{}
+		match mut node {
+			NumberLiteral {
+				child = NumberLiteral{
+					value: node.value
 				}
-				else {}
+			}
+			Call {
+				mut expression := &CallExpression{
+					callee: Identifier{
+						name: node.name
+					}
+				}
+				node.ctx = expression
+				if parent is Call {
+					child = expression
+				} else {
+					child = ExpressionStatement{
+						expression: expression
+					}
+				}
+			}
+			else {
+				panic('child node is unknown ? $node.type_name()')
 			}
 		}
-		/*
-		for e in node.body {
-			if mut e is Program {
-				the_ctx := e.ctx
-				println('prog ctx=${voidptr(the_ctx)}')
+		mut ctx := &ASTNode{}
+		match mut parent {
+			Program {
+				ctx = parent.ctx
 			}
-			if mut e is CallExpression {
-				the_ctx := e.ctx
-				println('callex ctx=${voidptr(the_ctx)}')
+			Call {
+				ctx = parent.ctx
 			}
-			// ret_ctx := 
-			e = traverse_node(e, node)
-			if mut e is Program {
-				the_ctx := e.ctx
-				println('prog ctx=${voidptr(the_ctx)}')
-			}
-			if mut e is CallExpression {
-				the_ctx := e.ctx
-				println('callex ctx=${voidptr(the_ctx)}')
+			else {
+				panic('parent is unknown ? ${typeof(parent).name}')
 			}
 		}
-		*/
-		/*
-		if mut node.ctx is Program {
-			node.ctx.body << NumberLiteral{
-				value: '5678'
+		match mut ctx {
+			Program {
+				ctx.body << child
+			}
+			CallExpression {
+				ctx.arguments << child
+			}
+			else {
+				panic('unknown program parent ctx $ctx.type_name()')
 			}
 		}
-		*/
-	} else if mut node is CallExpression {
-		println(' traverse Callex')
-		for i := 0; i < node.params.len; i++ {
-			// println('about to traverse from callexp e=${voidptr(e)}')
-			println('about to traverse from callexp')
-			node.params[i] = traverse_node(node.params[i], mut &node)
-		}
-		/*
-		} else if node.u.@type=='NumberLiteral'||node.u.@type=='StringLiteral' {
-		// nothing special
 	} else {
-		panic('Type error: `${node.u.@type}`')
-		*/
+		match node {
+			Program {}
+			else {
+				panic('null parent for node ${typeof(node).name}')
+			}
+		}
 	}
-	// return node.ctx
+	match mut node {
+		Program {
+			for mut e in node.body {
+				e = traverse_node(e, &node)
+			}
+		}
+		Call {
+			for mut e in node.params {
+				e = traverse_node(e, &node)
+			}
+		}
+		NumberLiteral {}
+		else {
+			panic('node is unknown ? ${typeof(node).name}')
+		}
+	}
 	return node
 }
 
-fn traverser(mut ast ASTNode) {
-	// ast.ctx = 
-	mut ptr := voidptr(0)
-	ast = traverse_node(ast, mut ptr)
-	/*
+fn transformer(mut ast ASTNode) (ASTNode, ASTNode) {
+	mut newast := &ASTNode(Program{})
 	if mut ast is Program {
-		if mut ast.ctx is Program {
-			ast.ctx.body << NumberLiteral{
-				value: '1234'
-			}
-			// if mut node.ctx is Program {
-			ast.ctx.body << ExpressionStatement{
-				expression: &Call{
-					@type: 'Identifier'
-					name: 'toto'
-				}
-			}
-			//}
-		}
+		ast.ctx = voidptr(newast)
 	}
-	*/
-}
-
-fn transformer(mut ast ASTNode) &ASTNode {
-	mut newast := &Program{}
-	if mut ast is Program {
-		ast.ctx = newast
-		println('ast.ctx=${voidptr(ast.ctx)}')
-	}
-	traverser(mut ast)
-	return newast
-}
-
-fn code_generator_v(node ASTNode) {
-	match node {
-		Program {
-			println('Program')
-		}
-		else {
-			panic('gen: unsupported node type')
-		}
-	}
+	ast = traverse_node(ast, voidptr(0))
+	return *ast, *newast
 }
 
 source := '(write(+ (* (/ 9 5) 60) 32))'
 tokens := tokenizer(source)
 // println(tokens)
 mut ast := parser(tokens)
-// println(ast)
+mut newast := ASTNode{}
+ast, newast = transformer(mut &ast)
 print_ast(ast)
-newast := transformer(mut ast)
+// println('ast=$ast')
 print_ast(newast)
-// println(newast)
-code_generator_v(newast)
+// println('newast=$newast')
