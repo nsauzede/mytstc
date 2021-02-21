07bb8222597debbd39ea37e3635742827a5579b5
Nicolas Sauzede <nsauzede@laposte.net>
2021-02-21 04:30:33 +0100

    Make C backend obj/list/print support on par with V

--- tstc.v
+++ tstc.v
@@ -441,11 +441,26 @@ fn (ctx Context) code_generator_c(node ASTNode) string {
 			if ctx.use_print {
 				sb.writeln('#include <stdio.h>')
 			}
+			if ctx.use_list {
+				sb.writeln('#include <stdarg.h>')
+				sb.writeln('#include <malloc.h>')
+			}
 			if use_obj {
-				sb.writeln('typedef enum{obj_f,obj_i,obj_s} obj_t;')
-				sb.writeln('typedef struct{obj_t t;union{float f;int i;char *s;};} obj;')
+				sb.writeln('typedef enum{obj_f,obj_i,obj_s,obj_l} obj_t;')
+				sb.writeln('
+struct obj;
+typedef struct {size_t l; struct obj *p;} List;
+typedef struct obj {obj_t t;union{float f;int i;char *s; List l;};} obj;
+				')
 				sb.writeln('#define F(v)(obj){obj_f,.f=v}')
 				sb.writeln('#define S(v)(obj){obj_s,.s=v}')
+
+				'
+#define PP_NARG(...) PP_NARG_(__VA_ARGS__,PP_RSEQ_N())
+#define PP_NARG_(...) PP_ARG_N(__VA_ARGS__)
+#define PP_ARG_N( _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, _31,_32,_33,_34,_35,_36,_37,_38,_39,_40, _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, _61,_62,_63,  N, ...) N
+#define PP_RSEQ_N() 63,62,61,60,                   59,58,57,56,55,54,53,52,51,50, 49,48,47,46,45,44,43,42,41,40, 39,38,37,36,35,34,33,32,31,30, 29,28,27,26,25,24,23,22,21,20, 19,18,17,16,15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+'
 			}
 			if ctx.use_add {
 				sb.writeln('obj add(obj a,obj b){obj r=F(0);r.f=a.f+b.f;return r;}')
@@ -461,17 +476,49 @@ fn (ctx Context) code_generator_c(node ASTNode) string {
 				sb.writeln('obj divide(obj a, obj b) {obj r=F(0);r.f=a.f/b.f;return r;}')
 			}
 			if ctx.use_print {
-				sb.writeln('void println(obj a){
+				sb.writeln('
+void println_(obj a, int depth){
 	if (a.t == obj_f){
-		printf("%f\\n", (double)a.f);
+		printf("%f", (double)a.f);
 	}
 	else if (a.t == obj_s){
-		printf("%s\\n", a.s);
+		printf("%s", a.s);
+	}
+	else if (a.t == obj_l){
+		printf("[");
+		for (int i = 0; i < a.l.l; i++) {
+			if (i > 0) {
+				printf(", ");
+			}
+			println_(a.l.p[i], depth + 1);
+		}
+		printf("]");
 	}
 	else{
-		printf("unknown obj type %d\\n", a.t);
+		printf("<unknown obj type %d>", a.t);
 	}
+	if (!depth) {
+		printf("\\n");
+	}
+}
+void println(obj a){
+	println_(a, 0);
 }')
+				if ctx.use_list {
+					sb.writeln('
+obj list(int n, ...) {
+	obj r = {obj_l, .l={0, 0}};
+	va_list args;
+	va_start(args, n);
+	for (int i = 0; i < n; i++) {
+		size_t new_len = r.l.l + 1;
+		r.l.p = realloc(r.l.p, (r.l.l + 1) * sizeof(obj));
+		r.l.p[r.l.l++] = va_arg(args, obj);
+	}
+	va_end(args);
+	return r;
+}')
+				}
 			}
 			sb.writeln('int main() {')
 			for e in node.body {
@@ -503,8 +550,15 @@ fn (ctx Context) code_generator_c(node ASTNode) string {
 			sb.write(name)
 		}
 		CallExpression {
-			sb.write(ctx.code_generator_c(node.callee))
+			callee := ctx.code_generator_c(node.callee)
+			sb.write(callee)
 			sb.write('(')
+			if callee == 'list' {
+				sb.write('$node.arguments.len')
+				if node.arguments.len > 0 {
+					sb.write(', ')
+				}
+			}
 			for i, e in node.arguments {
 				if i > 0 {
 					sb.write(', ')
